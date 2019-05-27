e865570bc6f992fdedec29ec3f56d840d8960ced
Amber Meritt <amber.merittcampbell@gmail.com>
2019-05-26 17:22:22 -0700

    updated wireframe to homepage content

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log("index.js attached")
+//User can see question they are on and their current score.
+//User enters quiz
+//    displays question and answer options
+//    updates question count 
+//user should be able to submit answer
+//    checks if it is true
+//    updates score 
+//    updates question count 
+//    displays next question and answer options 
+//At the end of array displays results 
+//    return to the beginning
+
