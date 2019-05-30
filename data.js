4bb4d1530ee01739008971d753b31cb0bc73cb93
Amber Meritt <amber.merittcampbell@gmail.com>
2019-05-29 18:45:46 -0700

    js added, user testing

---
+++ data.js
@@ -0,0 +1,114 @@
+const questionData = [
+    {
+        question: "Who is Ororo Munroe?",
+        A: "Rogue",
+        B: "Psylocke",
+        C: "Storm",
+        D: "Jubilee",
+        correctAnswer: "Storm"
+    },
+    {
+        question: "Who did Rogue first kiss?",
+        A: "Gambit",
+        B: "David",
+        C: "Iceman",
+        D: "Cody",
+        correctAnswer: "Cody"
+    },
+    {
+        question: "Who does Cyclops first marry?",
+        A: "Madelyne Pryor",
+        B: "Jean Grey",
+        C: "Rogue",
+        D: "Storm",
+        correctAnswer: "Madelyne Pryor"
+    },
+    {
+        question: "Before the X-Men, what group was Gambit apart of?",
+        A: "X-Force",
+        B: "Thieves' Guild",
+        C: "Rogue's Guild",
+        D: "X-Factor",
+        correctAnswer: "Thieves' Guild"
+    },
+    {
+        question: "What X-Men looked up to Wolverine?",
+        A: "Rogue",
+        B: "Kitty Pryde",
+        C: "Jubilee",
+        D: "X-23",
+        correctAnswer: "Jubilee"
+    },
+    {
+        question: "Before becoming Phoenix, what was Jean Grey's alias?",
+        A: "Marvel Girl",
+        B: "Ms. Marvel",
+        C: "Captain Marvel",
+        D: "Mar'vel",
+        correctAnswer: "Marvel Girl"
+    },
+    {
+        question: "Who is Charles Xavier's brother?",
+        A: "Juggernaut",
+        B: "Shadow King",
+        C: "Magneto",
+        D: "Banshee",
+        correctAnswer: "Juggernaut"
+    },
+    {
+        question: "what is Beast's real name?",
+        A: "Henry 'Hank' McCoy",
+        B: "Kurt Wagner",
+        C: "Erik 'Joseph' Lehnsherr",
+        D: "Remy LeBeau",
+        correctAnswer: "Henry 'Hank' McCoy"
+    },
+    {
+        question: "Who is Nightcrawler's father?",
+        A: "Dr. Strange",
+        B: "Arnos",
+        C: "Azazel",
+        D: "Stefan",
+        correctAnswer: "Azazel"
+    },
+    {
+        question: "Who is Nightcrawler's mother?",
+        A: "Jardine",
+        B: "Magik",
+        C: "Margali",
+        D: "Mystique",
+        correctAnswer: "Mystique"
+    },
+]
+
+const correctResults = [
+    {
+        statement: "Congratulations! You knocked Rogue down!",
+        image: "https://i.makeagif.com/media/10-06-2015/lyuTeh.gif",
+        alt: "Rogue falls down"
+    },
+    {
+        statement: "Congratulations! You threw Rogue far!",
+        image: "https://thumbs.gfycat.com/SlipperyPoliteGemsbuck-size_restricted.gif",
+        alt: "Rogue is thrown into boulders"
+    },
+    
+]
+
+const wrongResults = [
+    {
+        statement: "Rogue Knocked you out! Better luck next time.",
+        image: "https://68.media.tumblr.com/416eee7b302bd6581976415c68f82cb7/tumblr_o6qzdunFiH1sgixyoo1_500.gif",
+        alt: "Rogue punches you"
+    },
+    {
+        statement: "Rogue absorbed your powers. Watch out for her hands!",
+        image: "https://media1.tenor.com/images/f89881acd0b30049a29af8bed01846df/tenor.gif?itemid=5056826",
+        alt: "Rogue absorbs your powers"
+    },
+    {
+        statement: "Rogue throws you across the room. Thanks Carol...",
+        image: "https://thumbs.gfycat.com/BreakableSeparateAtlanticbluetang-size_restricted.gif",
+        alt: "Rogue throws you"
+    },
+]
--- index.js
+++ index.js
@@ -1,13 +1,187 @@
 console.log("index.js attached")
-//User can see question they are on and their current score.
-//User enters quiz
-//    displays question and answer options
-//    updates question count 
-//user should be able to submit answer
-//    checks if it is true
-//    updates score 
-//    updates question count 
-//    displays next question and answer options 
-//At the end of array displays results 
-//    return to the beginning
+//Start Quiz
+//  render question
+//  increase question count
+//Submit Answer
+//  test is answer is correct
+//  display results
+// update score
+//Next question
+//  test if question is less than 10
+//  false, render next question and update count
+//  true, show score
+//Restart Quiz
 
+let num = 0;
+let score = 0;
+
+function generateQuestion(){
+    return `<form>
+        <div class="question">
+            <p>${questionData[num].question}</p>
+        </div>
+        <div class="multiChoice answers">
+            <label>
+                <input class="answer" type="radio" name="option" value="${questionData[num].A}" checked></input>
+                <span>${questionData[num].A}</span>
+            </label>
+            <br>
+            <label>
+                <input class="answer" type="radio" name="option" value="${questionData[num].B}"></input>
+                <span>${questionData[num].B}</span>
+            </label>
+            <br>
+            <label>
+                <input class="answer" type="radio" name="option" value="${questionData[num].C}"></input>
+                <span>${questionData[num].C}</span>
+            </label>
+            <br>
+            <label>
+            <input class="answer" type="radio" name="option" value="${questionData[num].D}"></input>                    <span>${questionData[num].D}</span>
+            </label>
+        </div>
+        <input type="submit" class ="js-submitAnswer" value="Submit Answer">
+    </form>`;
+}
+
+function renderQuestion(){
+    $('.formContainer').html(generateQuestion());
+    console.log("Displays the question");
+}
+
+function increaseQuestionCounter(){
+    num ++
+    $('.questionCount').text(num);
+    console.log(`Question count is ${num}`);
+}
+
+function handleStartQuiz(){
+    $('form').submit(function(event){
+        event.preventDefault();
+        renderQuestion();
+        increaseQuestionCounter();
+    });
+}
+
+function isAnswerCorrect(){
+    let selected = $('input:checked').val();
+    let answer = `${questionData[num-1].correctAnswer}`;
+    if(selected === answer){
+        return true;
+    } else {
+        return false;
+    };
+}
+
+function pickRandomResult(obj){
+    return obj[Math.floor(Math.random()*obj.length)];
+}
+
+function handleScore(){
+    score ++
+    $('.scoreCount').text(score);
+    console.log(`Score is ${score}`);
+}
+
+function generateResults(){
+    let userCorrect = isAnswerCorrect();
+    if(userCorrect === true){
+        handleScore();
+        let randomResult = pickRandomResult(correctResults);
+        return `<form>
+            <div class="question">
+                <p>${randomResult.statement}</p>
+            </div>
+            <div class="multiChoice home">
+                <img src="${randomResult.image}" alt="${randomResult.alt}">
+            </div>
+            <input type="submit" class ="js-next" value="Next">
+        </form>`;
+    } else {
+        let randomResult = pickRandomResult(wrongResults);
+        return `<form>
+            <div class="question">
+                <p>${randomResult.statement}</p>
+                <p>The correct answer is ${questionData[num-1].correctAnswer}.</p>
+            </div>
+            <div class="multiChoice home">
+                <img src="${randomResult.image}" alt="${randomResult.alt}">
+            </div>
+            <input type="submit" class ="js-next" value="Next">
+        </form>`;
+    }
+}
+
+function renderResults(){
+    $('.formContainer').html(generateResults());
+}
+
+function handleSubmitAnswer(){
+    $('.formContainer').on('click', '.js-submitAnswer', function(event){
+        event.preventDefault();
+        renderResults();
+    });
+}
+
+function userScore(){
+    return score / 10 * 100;
+}
+
+function generateScorePage(){
+    let grade = userScore();
+    if(grade < 70) {
+        return `<form>
+        <div class="question">
+            <p>Looks like you need to spend more time in the Danger Room. Better luck next time.</p>
+        </div>
+        <div class="multiChoice home">
+            <img src="https://orig00.deviantart.net/2f59/f/2018/245/7/7/the_uncanny__xmen_by_blackrocklegacies-dclswco.jpg" alt="X-Men Logo">
+        </div>
+        <input type="submit" class ="js-startQuiz" value="Enter the Danger Room">
+        </form>`;
+    }else {
+        return `<form>
+        <div class="question">
+            <p>Congratulations! You defeated Rogue!</p>
+        </div>
+        <div class="multiChoice home">
+            <img src="https://media1.tenor.com/images/8e38795838c0f6d79e3bff5553624207/tenor.gif?itemid=7797140" alt="Rogue image">
+        </div>
+        <input type="submit" class ="js-startQuiz" value="Enter the Danger Room">
+        </form>`;
+    }
+}
+
+function renderScorePage(){
+    $('.formContainer').html(generateScorePage());
+}
+
+function handleNext(){
+    $('.formContainer').on('click', '.js-next', function(event){
+        event.preventDefault();
+        console.log(userScore());
+        if (num >= 10){
+            renderScorePage();
+        }else {
+            renderQuestion();
+            increaseQuestionCounter();
+            console.log("User went to the next question");
+        }
+    });
+}
+
+function handleRestartQuiz(){
+    $('form').submit(function(event){
+        event.preventDefault();
+        console.log("User restarted the quiz");
+    });
+}
+
+function handleSubmits(){
+    handleStartQuiz();
+    handleSubmitAnswer();
+    handleNext();
+    handleRestartQuiz();
+}
+
+handleSubmits();
